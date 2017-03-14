ff88d588c5bbf3ad1046184e2287f8f2cec7bdea
liridho tif16c <liridho@outlook.com>
2017-03-14 21:11:15 +0700

    Add files via upload

---
+++ praktek3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define PHI 3.14 // konstanta
+
+using namespace std;
+int main() {
+	float jari_jari, luas;
+
+	//konstanta
+	jari_jari = 25;
+
+luas = 0.5*PHI*jari_jari*jari_jari;
+cout<<"Data Lingkaran";
+cout<<"\nJari-jari : "<<jari_jari;
+cout<<"nLuas : "<<luas;
+cout<<"\n";
+return 0;
+}
