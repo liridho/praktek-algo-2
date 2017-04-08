f709cfdce7fc6e6db9ced9171b53a488bc23048a
liridho tif16c <liridho@outlook.com>
2017-04-08 21:20:20 +0700

    Add files via upload

---
+++ tugas membandingkan nilai.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int A,B;
+
+
+int main() {
+cout<<"============================="<<endl;
+cout<<"PROGRAM MEMBANDINGKAN 2 NILAI"<<endl;
+cout<<"============================="<<endl;
+cout<<"Masukkan nilai A : "; cin>>A;
+cout<<endl;
+cout<<"Masukkan nilai B : "; cin>>B;
+cout<<endl;
+cout<<"=============================="<<endl;
+
+if(A>B) { cout<<"A lebih besar dari B"<<endl;}
+else if(A==B) {cout<<"A sama dengan B"<<endl;}
+else{cout<<"B lebih besar dari A"<<endl;}
+cout<<"author : LIRIDHO, TIF RM 16 C, 16.111.212";
+}
