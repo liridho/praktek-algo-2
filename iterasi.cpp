fa2b2ecf0ef04b6d6e3108006a423f5e8d348a97
liridho tif16c <liridho@outlook.com>
2017-06-07 16:00:44 +0700

    Add files via upload

---
+++ iterasi.cpp
@@ -0,0 +1,12 @@
+//iterasi
+#include<iostream>
+using namespace std;
+void numberFunction(int i) {
+cout<<"The number is : " <<i<<endl;
+}
+int main () {
+for (int i=0;i<10;i++) {
+numberFunction(i);
+}
+return 0;
+}
